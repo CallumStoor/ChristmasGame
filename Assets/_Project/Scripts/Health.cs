e1f0fa59bd6338c6fafc478ad83521322a977de8
CallumStoor <562395@escg.ac.uk>
2025-12-18 09:41:47 +0000

    PlayerController

---
+++ Assets/_Project/Scripts/Health.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
