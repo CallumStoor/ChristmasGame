0ced67a37567ce7b3f0e39f4599a014d5fd09c01
CallumStoor <562395@escg.ac.uk>
2025-12-18 16:13:05 +0000

    l;

--- Assets/_Project/Scripts/Finish.cs
+++ Assets/_Project/Scripts/Finish.cs
@@ -7,6 +7,6 @@ public class Finish : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene(SceneManager.sceneCount + 1);
+        SceneManager.LoadScene("Scene2");
     }
 }
